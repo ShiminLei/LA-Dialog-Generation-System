76a1b21e417875afb0455502d5d13f7f71c66bc5
ShiminLei <sl2kd@virginia.edu>
2019-11-16 17:24:15 -0500

    new

--- uva_run.sh
+++ uva_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-nohup python ptb-utt.py --max_epoch 1 --ckpt_step 100 &
+nohup python ptb-utt.py --max_epoch 1 --ckpt_step 100 &> /dev/null &
 sleep 2s
-nohup python ptb-utt.py --max_epoch 1 --ckpt_step 100 &
+nohup python ptb-utt.py --max_epoch 1 --ckpt_step 100 &> /dev/null &
 sleep 2s
-nohup python ptb-utt.py --max_epoch 1 --ckpt_step 100 &
+nohup python ptb-utt.py --max_epoch 1 --ckpt_step 100 &> /dev/null &
 
 
 #$ chmod +x uva_run.sh
