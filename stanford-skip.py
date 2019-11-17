b475d8261e58fb754cbf3686af453e18dae176c3
ShiminLei <sl2kd@virginia.edu>
2019-11-16 22:32:37 -0500

    new

--- stanford-skip.py
+++ stanford-skip.py
@@ -144,10 +144,12 @@ def main(config):
     selected_outs = dialog_utils.selective_generate(model, test_feed, config, selected_clusters)
     print(len(selected_outs))
 
-    with open(os.path.join(dump_file+'.json'), 'wb') as f:
+    # with open(os.path.join(dump_file+'.json'), 'wb') as f:
+    with open(os.path.join(dump_file + '.json'), 'w') as f:
         json.dump(selected_clusters, f, indent=2)
 
-    with open(os.path.join(dump_file+'.out.json'), 'wb') as f:
+    # with open(os.path.join(dump_file+'.out.json'), 'wb') as f:
+    with open(os.path.join(dump_file + '.out.json'), 'w') as f:
         json.dump(selected_outs, f, indent=2)
 
 
